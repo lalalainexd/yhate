96b8a59576ba768abe1c9a4ef2844a1b070817e8
lalalainexd <lalalainexd@gmail.com>
2013-06-02 10:55:02 -0600

    began app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyStory::Application.config.secret_token = 'c349b13d0f1fb2afc06c18982cd27c4b06565cfcde21f9ed0c451957d2573fd80a859cce5b1cd46426492962dd4c6c1a712620d1a1ca8d65e5f33c41912c68c0'
