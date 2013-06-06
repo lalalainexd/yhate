c86b6d3df3b2598f4014dd6994156fd7c5c8ba14
lalalainexd <lalalainexd@gmail.com>
2013-06-06 13:05:00 -0600

    added some stories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+story = Story.new
+story.occurred_at = "May 18, 2013"
+story.victims = "Mark Carson"
+story.link = "http://www.nytimes.com/2013/05/30/nyregion/mark-carson-gay-man-killed-in-greenwich-village-is-remembered.html?_r=0"
+story.title = "Shot"
+story.bias = "anti-male homosexual"
+story.offense = "murder"
+story.description = "Mark Carson, was a hard worker, party lover, momma's boy, and christian. Despite being gay, Mark was very religious and always working to please God. On May 18, he was shot because of his sexual orientation."
+story.save!
+
+story = Story.new
+story.occurred_at = "May 18, 2013"
+story.victims = "Chris, Cee"
+story.link = "http://www.koaa.com/news/gay-soldiers-beaten-in-possible-hate-crime/#_"
+story.title = "Shot"
+story.bias = "anti-homosexuality"
+story.offense = "aggravated assault"
+story.description = "Chris, Cee, and some friends were brutally attacked and beaten for being gay. 'We walked in and immediately one [guy] started making remarks like, faggot'"
+story.save!
+
+story = Story.new
+story.occurred_at = "August 26, 2012"
+story.victims = "Zachary Tennen"
+story.link = "http://archive.adl.org/PresRele/ASUS_12/6375_12.htm"
+story.title = "Stapled Shut"
+story.bias = "anti-jewish"
+story.offense = "aggravated assault"
+story.description = "Zach, a sophomore attending Michigan state University was knocked unconscious and had his mouth stapled shut by two men at a party, after the men allegedly asked the student if he was Jewish, chanted 'Heil Hitler' and gave the Nazi salute."
+story.save!
